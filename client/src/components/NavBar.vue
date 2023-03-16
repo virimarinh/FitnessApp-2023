e53d8152e3947fb3c690b206023856113cef9009
Viri Marin <marinhev1@newpaltz.edu>
2023-03-16 12:59:18 -0400

    Navbar updates

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -4,13 +4,12 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <nav class="navbar is-primary">
+    <nav class="navbar is-danger">
         <div class="container">
         <div class="navbar-brand">
           <a class="navbar-item" href="https://bulma.io">
             
             <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="22" height="22" />
-            
           </a>
           <div class="navbar-burger" :class="{ 'is-active': isMenuActive }" @click="isMenuActive = !isMenuActive" >
             <span></span>
@@ -22,36 +21,27 @@ import { RouterLink } from 'vue-router';
         <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
           <div class="navbar-start">
 
-            <RouterLink to="/" class="navbar-item">Home</RouterLink>
-            <RouterLink to="/about" class="navbar-item">About</RouterLink>
+            <RouterLink to="/" class="navbar-item">
+              <i class="fa-solid fa-person-walking"></i>
+               My Activity
+            </RouterLink>
+            <RouterLink to="/about" class="navbar-item">
+              <i class="fa-light fa-chart-line"></i>
+              Statistics
+            </RouterLink>
+            <RouterLink to="/about" class="navbar-item">
+              <i class="fa-solid fa-users"></i>
+              Friends Activity
+            </RouterLink>
 
 
             <div class="navbar-item has-dropdown is-hoverable">
               <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
-                Docs
+                Admin
               </a>
               <div class="navbar-dropdown">
                 <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
-                  Overview
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
-                  Modifiers
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                  Columns
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                  Layout
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                  Form
-                </a>
-                <hr class="navbar-divider">
-                <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                  Elements
-                </a>
-                <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                  Components
+                  Users
                 </a>
               </div>
             </div>
@@ -61,6 +51,19 @@ import { RouterLink } from 'vue-router';
             <div class="navbar-item">
               <div class="field is-grouped">
                 <p class="control">
+                  <a class="button is-danger" href="">
+                    <span>Sign Up</span>
+                  </a>
+                </p>
+                <p class="control">
+                  <a class="button" href="">
+                    <span>Login</span>
+                    <span class="icon is-small">
+                      <i class="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                </p>
+                <p class="control">
                   <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
                     <span class="icon">
                       <i class="fab fa-twitter"></i>
@@ -68,14 +71,6 @@ import { RouterLink } from 'vue-router';
                     <span>
                       Tweet
                     </span>
-                  </a>
-                </p>
-                <p class="control">
-                  <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.3/bulma-0.9.3.zip">
-                    <span class="icon">
-                      <i class="fas fa-download"></i>
-                    </span>
-                    <span>Download</span>
                   </a>
                 </p>
               </div>
