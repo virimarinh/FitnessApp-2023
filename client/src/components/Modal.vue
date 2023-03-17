b5abe33f2f2475cadc8e5564b3d9333a4331d1da
Viri Marin <marinhev1@newpaltz.edu>
2023-03-17 06:57:53 -0400

    Created Modal

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="modal"> 
+        <div class="modal-background"></div>
+        <header class="modal-card-header">
+          <p class="modal-card-title">Add Workout</p>
+          <button class="close">Close</button>
+        </header>
+        <section class="modal-card-body">
+          <form>
+        <div class="field">
+          <label for="name">Type</label>
+          <div class="control">
+            <input type="text" class="input">
+          </div>
+        </div>
+        <div class="field">
+          <label for="name">Date</label>
+          <div class="control">
+            <input type="text" class="input" placeholder="mm/dd/yyyy">
+          </div>
+        </div>
+        <div class="field">
+          <label for="name">Duration</label>
+          <div class="control">
+            <input type="text" class="input">
+          </div>
+        </div>
+        <div class="field">
+          <label for="name">Picture</label>
+          <div class="control">
+            <input type="text" class="input">
+          </div>
+        </div>
+        <div class="select is-normal">
+            <label>Type</label>
+            <select>
+                <option></option>
+                <option>Run</option>
+                <option>Walk</option>
+                <option>Bike</option>
+                <option>Cardio</option>
+            </select>
+        </div>
+        <div class="footer">
+            <button>Save Workout</button>
+            <button>Cancel</button>
+        </div>
+      </form>
+        </section>
+        <footer class="nodal-card-footer">
+          <button class="button is-warning">Save Workout</button>
+          <button class="button">Cancel</button>
+        </footer>
+      </div>
+</template>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
 import LoginVue from '@/views/Login.vue'
 import { useSession } from '@/model/session'
+import HomeView from '@/views/HomeView.vue'
 import ActivityView from '../views/ActivityView.vue'
 import StatisticsVue from '../views/Statistics.vue'
 import FriendsActivity from '../views/FriendsActivity.vue'
@@ -9,6 +10,7 @@ import PeopleSearch from '@/views/PeopleSearch.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', name: 'home', component: HomeView },
     { path: '/activity', name: 'activity', component: ActivityView},
     { path: '/login', name: 'login', component: LoginVue},
     { path: '/friends-activity', name: 'friends-activity', component: FriendsActivity},
