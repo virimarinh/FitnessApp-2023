1b0c1ce79c2b073ffd7431fd26ca0ff89bd991af
Viri Marin <marinhev1@newpaltz.edu>
2023-03-17 06:57:38 -0400

    Created Modal

--- client/src/views/ActivityView.vue
+++ client/src/views/ActivityView.vue
@@ -12,23 +12,5 @@ const showModal = ref(true)
         <button class="button is-danger is-fullwidth">Add Workout</button>
       </div>
     </div>
-    <div class="modal">
-      <div class="madal-content has-background py-5 px-5"></div>
-      <h3 class="title"></h3>
-      <form>
-        <div class="field">
-          <label for="name">Type</label>
-          <div class="control">
-            <input type="text" class="input">
-          </div>
-        </div>
-        <div class="field">
-          <label for="name"></label>
-          <div class="control">
-            <input type="text" class="input">
-          </div>
-        </div>
-      </form>
-    </div>
   </div>
 </template>
