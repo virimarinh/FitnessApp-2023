dcbd170f18331b9136c8eb7036cc7c60626d19d9
Viri Marin <marinhev1@newpaltz.edu>
2023-03-17 08:42:53 -0400

    Workout Summary

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,12 +6,48 @@ import Modal from '@/components/Modal.vue';
 
 <template>
   <main>
-    <div class="container">
-      <div class="columns is multiline">
-        <div class="column is-one-half">
-          h3
-        </div>
-      </div>
-    </div>
+    <div class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-one-quarter">
+                        <div class="card">
+                          </div>
+                        </div>
+
+                    <div class="column is-half">
+                        <div class="column">
+                          <div class="box has-text-danger">
+                            <h2 class="title">Today</h2>
+                            <div class="columns is-multiline">
+                              <div class="column is-half">
+                                <h3 class="value">0 ft</h3>
+                                <caption class="caption">Distance</caption>
+                              </div>
+                              <div class="column is-half">
+                                <h3 class="value">0:0</h3>
+                                <caption class="caption">Duration</caption>
+                              </div>
+                              <div class="column is-half">
+                                <h3 class="value">NaN mph</h3>
+                                <caption class="caption">Avg Pace</caption>
+                              </div>
+                              <div class="column is-half">
+                                <h3 class="value">0</h3>
+                                <caption class="caption">Calories</caption>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        
+                    <div class="column is one-quarter">
+                    </div>
+                </div>
+            </div>
+           </div>
+           </div>
   </main>
 </template>
+
+<style scoped>
+
+</style>
