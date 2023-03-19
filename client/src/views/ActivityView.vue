6e68c50b26f43603727528c45ca11a0852eec283
Viri Marin <marinhev1@newpaltz.edu>
2023-03-19 06:59:09 -0400

    addend Modal button functionality and media boxes

--- client/src/views/ActivityView.vue
+++ client/src/views/ActivityView.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
+import { modalActive, toggleModal, closeModal } from '@/model/modal';
 
-const showModal = ref(true)
 </script>
-
 <template> 
   <div class="container">
     <h1 class="title">My Activity</h1>
     <div class="columns">
       <div class="column is-half is-offset-one-quarter">
-        <button class="button is-danger is-fullwidth">Add Workout</button>
+       <Modal :modalActive="modalActive"></Modal>
+        <button @click="toggleModal" class="button is-danger is-fullwidth">Add Workout</button>
         <br>
         <article class="media">
           <figure class="media-left">
