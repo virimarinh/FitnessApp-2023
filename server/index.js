a9f289355e2c33aeb07871e41b3f5e7575e24a1b
Viri Marin <marinhev1@newpaltz.edu>
2023-05-03 01:41:35 -0400

    moved tools folder outside of server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path');
-const exercises = require('./controllers/exercises');
+const exercises = require('./controllers/exercises')
 const users = require('./controllers/users')
 const { requireLogin, parseAuthorizationHeader } = require('./middleware/authorization')
 const app = express()
@@ -10,7 +10,6 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 //Middleware
-
 app
     .use(express.json())
     .use(express.static(path.join(__dirname, '../client/dist')))
@@ -19,10 +18,10 @@ app
     .use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        //Make sure OPTIONS request are allowed 
-        // That way pre-flight requests dont fail
-        if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        // Male sure OPTIONS request are always allowed
+        // That way pre-flight requests don't fail
+        if(req.method === 'OPTIONS') {
             return res.status(200).send({})
         }
         next()
@@ -30,37 +29,36 @@ app
 
     .use(parseAuthorizationHeader)
 
-//Actions
+
+// Actions
 app 
-    .get('/api/v1/', (req, res) => {
+    .get('/api/v1', (req, res) => {
         res.send('Hello World! From Express')
     })
-    .use('/api/v1/exercises', requireLogin(), exercises)
+    .use('/api/v1/exercises', exercises)
     .use('/api/v1/users', users)
 
-//Catch All
-app
-    .get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-    })
-
-//Error handling
-app
-.use((err, req, res, next) => {
-    console.error(err);
-    const msg = {
-        status: err.code || 500,
-        error: err.message || 'Internal Server Error',
-        isSuccess: false
-    }
-    res.status(msg.status).json(msg)
+// Catch All
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirtname, '../client/dist/index.html'))
 })
 
+// Error Handling
+app
+    .use((err, req, res, next) => {
+        const msg = {
+            status: err.code || 500,
+            error: err.message || 'Internal Server Error',
+            isSuccess: false
+        }
+        res.status(msg.status).json(msg)
+    })
 
-console.log('1: About to start server')
 
-app.listen(port, () => 
-console.log(`2: Server running at http://${hostname}:${port}/`)
-);
+    console.log('1: About to start server')
 
-console.log('3: Asked server to start')
+    app.listen(port, () => 
+      console.log(`2: Server running at http://${hostname}:${port}/`)
+    );
+    
+    console.log('3: Asked server to start')
